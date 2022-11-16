cf1cfb3dd73058a9d32b63dc2f426d08289cc405
damonbuzzni <damon@buzzni.com>
2022-11-16 17:59:02 +0900

    client init and post doc by use requests

--- bbm/__init__.py
+++ bbm/__init__.py
@@ -1,2 +1,127 @@
+import socket
+import sys
+import time
+import traceback
+from datetime import datetime
+from functools import wraps
+from uuid import uuid4
+
+import pytz
+import requests
+
+from bbm.exceptions import BBMNotInitialized
+
 # package info
-__version__ = "0.0.1"
+__version__ = "0.0.2"
+
+KST = pytz.timezone("Asia/Seoul")
+
+
+def get_ip():
+    return requests.get("http://ipgrab.io").text
+
+
+def get_hostname():
+    return socket.gethostname()
+
+
+class BBM:
+    def __init__(self, es_url: str, index_prefix: str = "batch-process-log"):
+        self.ip = get_ip()
+        self.hostname = get_hostname()
+        self.es_url = es_url
+        self.index_prefix = index_prefix if index_prefix.endswith("-") else f"{index_prefix}-"
+
+    def post_log(
+        self,
+        process: str,
+        func: str,
+        param: dict,
+        level: str = "info",
+    ):
+        now_kst = datetime.now(tz=KST)
+        get_date_to_index = now_kst.strftime("%Y.%m.%d")
+        index = f"{self.index_prefix}{get_date_to_index}"
+        datetime_to_write_at_es = now_kst.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+        write_dict = {
+            "process": process,
+            "func": func,
+            "level": level,
+            "param": param,
+            "ip": self.ip,
+            "host": self.hostname,
+            "@timestamp": datetime_to_write_at_es,
+        }
+        try:
+            return requests.post(f"{self.es_url}/{index}/_doc", json=write_dict)
+        except Exception as e:
+            raise e
+
+
+bbm: BBM
+
+
+def setup(es_url: str, index_prefix: str = "batch-process-log"):
+    global bbm
+    bbm = BBM(es_url=es_url, index_prefix=index_prefix)
+
+
+def logging(
+    py_name,
+    interval=3600,
+):
+    def wrapper(func):
+        @wraps(func)
+        def decorator(*args, **kwargs):
+            if not bbm:
+                raise BBMNotInitialized("BBM is not initialized")
+            func_name = func.__name__
+            process_uuid = str(uuid4())
+            result = None
+            start_time = time.time()
+            start_log_param = {
+                "msg": "start",
+                "cate": "fission-tasks",
+                "interval": interval,
+                "process_uuid": process_uuid,
+            }
+            complete_log_param = {
+                "msg": "complete",
+                "cate": "fission-tasks",
+                "process_uuid": process_uuid,
+            }
+            try:
+                bbm.post_log(
+                    process=py_name,
+                    func=func_name,
+                    param=start_log_param,
+                )
+                result = func(*args, **kwargs)
+                duration = round(time.time() - start_time, 2)
+                complete_log_param["duration"] = duration
+                bbm.post_log(
+                    process=py_name,
+                    func=func_name,
+                    param=complete_log_param,
+                )
+            except Exception as e:
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                error_traceback = "\n".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+                duration = round(time.time() - start_time, 2)
+                bbm.post_log(
+                    process=py_name,
+                    func=func_name,
+                    level="error",
+                    param={
+                        "msg": "fail",
+                        "cate": "fission-tasks",
+                        "process_uuid": process_uuid,
+                        "duration": duration,
+                        "error_traceback": error_traceback,
+                    },
+                )
+            return result
+
+        return decorator
+
+    return wrapper
